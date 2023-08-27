bdfc5458e045b843602867e6a72c49819f7e5d1a
A.Protasevich <larsplotner@gmail.com>
2023-08-27 18:41:11 +0300

    feat: add test bad is increment

--- part_6/unicafe-redux/src/reducer.test.js
+++ part_6/unicafe-redux/src/reducer.test.js
@@ -47,4 +47,18 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+  test('bad is increment', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
 })
